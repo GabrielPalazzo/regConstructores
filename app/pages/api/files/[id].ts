3540a7ae88551dc4897694bc72035bb1ce3deda4
Victor Hugo Contreras <vcontreras@gmail.com>
2022-06-01 17:29:37 -0300

    Add try and catch (error) en IPFS

--- app/pages/api/files/[id].ts
+++ app/pages/api/files/[id].ts
@@ -5,51 +5,37 @@ import fs from 'fs'
 
 
 const endpoint = async (req, res) => {
+  try {
+    const { id: documentId, name } = req.query;
+    const fileName = 'rnc_private_doc.pdf';
 
-  const {
-    id: documentId,
-    name
-  } = req.query
+    // console.log(req.query.id);
+    const client = ipfsClient({
+      host: process.env.IPFS_NODE_HOST,
+      protocol: process.env.IPFS_NODE_PROTOCOL,
+      port: parseInt(process.env.IPFS_NODE_PORT, 10),
+      apiPath: process.env.IPFS_NODE_APIPATH
+    })
 
-  const fileName = 'rnc_private_doc.pdf'
+    const stream = client.cat(documentId);
 
+    let data = [];
+    for await (const chunk of stream) { data.push(chunk) }
 
-  console.log(req.query.id)
-  const client = ipfsClient({
-    host: process.env.IPFS_NODE_HOST,
-    protocol: process.env.IPFS_NODE_PROTOCOL,
-    port: parseInt(process.env.IPFS_NODE_PORT,10),
-    apiPath: process.env.IPFS_NODE_APIPATH
-  })
+    res.writeHead(200, {
+      'Content-Type': 'application/octet-stream',
+      'Content-Disposition': `attachment; filename=${name}`,
+      'Content-Length': Buffer.concat(data).length
+    });
+    res.end(Buffer.concat(data));
 
-  // const fileFetched = await client.cat(documentId)
-  // const fileFetched = await client.get(req.query.id)
-  // console.log(fileFetched)
-
-  const stream = client.cat(documentId)
-
-  let data = []
-  for await (const chunk of stream){
-    data.push(chunk)
   }
-
- 
- 
-  //const r = String.fromCharCode.apply(null, new Uint16Array(buffer))
-  //onsole.log(r)
-  res.writeHead(200, {
-    'Content-Type': 'application/octet-stream',
-    'Content-Disposition': `attachment; filename=${name}`,
-    'Content-Length': Buffer.concat(data).length
-  });
-  res.end(Buffer.concat(data));
-   
+  catch (error) {
+    console.log('#### Error endpoint get [id] ' + req.query.id + '. ' + error);
+    res.status(500).send(error)
+  }
 }
 
-export const config = {
-    api: {
-      bodyParser: false,
-    },
-  };
-  
-  export default endpoint
+export const config = { api: { bodyParser: false, }, };
+
+export default endpoint
--- app/pages/api/files/new.ts
+++ app/pages/api/files/new.ts
@@ -4,44 +4,45 @@ import fs from 'fs'
 
 
 const endpoint = async (req, res) => {
+  try {
+    let form = new multiparty.Form();
+    const result: Array<{
+      fileName: string
+      cid: string
+      createdAt: number
+    }> = []
 
-  let form = new multiparty.Form();
-  const result: Array<{
-    fileName: string
-    cid: string
-    createdAt: number
-  }> = []
+    form.parse(req, async (err, fields, files) => {
+      if (err)
+        console.log(err);
 
-  form.parse(req, async (err, fields, files) => {
-    if (err) console.log(err)
+      const client = ipfsClient({
+        host: process.env.IPFS_NODE_HOST,
+        protocol: process.env.IPFS_NODE_PROTOCOL,
+        port: parseInt(process.env.IPFS_NODE_PORT, 10),
+        apiPath: process.env.IPFS_NODE_APIPATH
+      })
 
-    const client = ipfsClient({
-      host: process.env.IPFS_NODE_HOST,
-      protocol: process.env.IPFS_NODE_PROTOCOL,
-      port: parseInt(process.env.IPFS_NODE_PORT,10),
-      apiPath: process.env.IPFS_NODE_APIPATH
-    })
-
-    for (let i = 0; i <= files.file.length - 1; i++) {
-      const f = files.file[i]
-      const binary = fs.readFileSync(f.path)
-      const { cid } = await client.add(binary)
+      for (let i = 0; i <= files.file.length - 1; i++) {
+        const f = files.file[i]
+        const binary = fs.readFileSync(f.path)
+        const { cid } = await client.add(binary)
         result.push({
           fileName: f.originalFilename,
           cid: cid.toString(),
           createdAt: new Date().getTime()
         })
         res.json({ filesSaved: result })
-    }
-  });
+      }
+    });
 
+  }
+  catch (error) {
+    console.log('#### Error endpoint file new ' + error);
+    res.status(500).send(error)
+  }
 }
 
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export const config = { api: { bodyParser: false, }, };
 
 export default endpoint
