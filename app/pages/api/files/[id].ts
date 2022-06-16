994a77edcc82f2c58db5596365c4250deae166af
Victor Hugo Contreras <vcontreras@gmail.com>
2022-06-16 17:42:44 -0300

    CONSD-114861 Mantenga sockets disponibles ipfs-client

--- app/pages/api/files/[id].ts
+++ app/pages/api/files/[id].ts
@@ -1,9 +1,14 @@
 import { ConsoleSqlOutlined } from '@ant-design/icons'
 import ipfsClient from 'ipfs-http-client'
 import fs from 'fs'
+import { Agent } from 'http';
 
-
-
+/** 
+ * https://www.npmjs.com/package/ipfs-http-client/v/55.0.1-7fe0da57f.0
+ * Code: agent: process.env.agent as any || new Agent({ keepAlive: true, maxSockets: 6 })
+ * CONSD-114861 Análisis logs 10/01/2022 para detección y corrección de error reportado en INFRAONC-2481
+ * INFRAONC-2601 Solicitud de archivos de logs de errores de RNC PRD del día 10/01/2022
+*/
 const endpoint = async (req, res) => {
   try {
     const { id: documentId, name } = req.query;
@@ -13,9 +18,10 @@ const endpoint = async (req, res) => {
       host: process.env.IPFS_NODE_HOST,
       protocol: process.env.IPFS_NODE_PROTOCOL,
       port: parseInt(process.env.IPFS_NODE_PORT, 10),
-      apiPath: process.env.IPFS_NODE_APIPATH
+      apiPath: process.env.IPFS_NODE_APIPATH,
+      agent: new Agent({ keepAlive: true, maxSockets: 6 })      
     })
-
+    
     const stream = client.cat(documentId);
 
     let data = [];
